02e3c2d85919e35f79435318601200422b727f52
Michal Grzadkowski <mgrzad@gmail.com>
2022-02-07 20:29:14 -0500

    making SCED model data extraction a part of model data

--- vatic/model_data.py
+++ vatic/model_data.py
@@ -21,7 +21,7 @@ class VaticModelData(object):
                  source: Union[dict, VModelData, str, Path,
                                None] = None) -> None:
         if isinstance(source, dict):
-            self._data = source
+            self._data = deepcopy(source)
 
         elif isinstance(source, VaticModelData):
             self._data = deepcopy(source._data)
@@ -52,7 +52,7 @@ class VaticModelData(object):
 
     def elements(self,
                  element_type: str,
-                 **element_args: str) -> Iterator[Tuple[str, Dict]]:
+                 **element_args) -> Iterator[Tuple[str, Dict]]:
         """Retrieves grid elements that match a set of criteria.
 
         Args
@@ -228,6 +228,263 @@ class VaticModelData(object):
                 'system']['reserve_requirement']['values'][time_index]
             self._data['system']['reserve_requirement']['values'][time_index] \
                 = max(reserve_factor * total_load, cur_req)
+
+    @property
+    def model_runtime(self):
+        return self._data['system']['solver_runtime']
+
+    @property
+    def duration_minutes(self):
+        return self._data['system']['time_period_length_minutes']
+
+    def get_max_power_output(self, gen):
+        gen_data = self._data['elements']['generator'][gen]
+
+        if isinstance(gen_data['p_max'], dict):
+            return gen_data['p_max']['values'][0]
+        else:
+            return gen_data['p_max']
+
+    @property
+    def thermal_fleet_capacity(self):
+        return sum(self.get_max_power_output(gen)
+                   for gen, _ in self.elements(element_type='generator',
+                                               generator_type='thermal'))
+
+    @property
+    def total_demand(self):
+        return sum(load_data['p_load']['values'][0]
+                   for load_data in self._data['elements']['load'].values())
+
+    @property
+    def fixed_costs(self):
+        return sum(gen_data['commitment_cost']['values'][0]
+                   for _, gen_data in self.elements(element_type='generator',
+                                                    generator_type='thermal'))
+
+    #TODO: allow for renewables to also have variable costs?
+    @property
+    def variable_costs(self):
+        return sum(gen_data['production_cost']['values'][0]
+                   for _, gen_data in self.elements(element_type='generator',
+                                                    generator_type='thermal'))
+
+    @property
+    def total_costs(self):
+        return self.fixed_costs + self.variable_costs
+
+    @property
+    def thermal_generation(self):
+        return {gen: gen_data['pg']['values'][0]
+                for gen, gen_data in self.elements(element_type='generator',
+                                                   generator_type='thermal')}
+
+    @property
+    def renewable_generation(self):
+        return {
+            gen: gen_data['pg']['values'][0]
+            for gen, gen_data in self.elements(element_type='generator',
+                                               generator_type='renewable')
+            }
+
+    @property
+    def load_shedding(self):
+        return sum(-bus_data['p_balance_violation']['values'][0]
+                   for bus_data in self._data['elements']['bus'].values()
+                   if bus_data['p_balance_violation']['values'][0] < 0.)
+
+    @property
+    def over_generation(self):
+        return sum(bus_data['p_balance_violation']['values'][0]
+                   for bus_data in self._data['elements']['bus'].values()
+                   if bus_data['p_balance_violation']['values'][0] > 0.)
+
+    @property
+    def reserve_shortfall(self):
+        if 'reserve_shortfall' in self._data['system']:
+            return self._data['system']['reserve_shortfall']['values'][0]
+        else:
+            return 0.
+
+    @property
+    def reserve_requirement(self):
+        if 'reserve_requirement' in self._data['system']:
+            return self._data['system']['reserve_requirement']['values'][0]
+        else:
+            return 0.
+
+    @property
+    def reserve_RT_price(self):
+        if 'reserve_price' in self._data['system']:
+            return self._data['system']['reserve_price']['values'][0]
+        else:
+            return 0.
+
+    @property
+    def available_reserve(self):
+        return {gen: (gen_data['headroom']['values'][0]
+                      + gen_data['rg']['values'][0])
+                for gen, gen_data in self.elements(element_type='generator',
+                                                   generator_type='thermal')}
+
+    @property
+    def available_quickstart(self):
+        return sum(
+            min(self._data['elements']['generator'][gen]['startup_capacity'],
+                self.get_max_power_output(gen))
+            for gen, gen_data in self.elements(element_type='generator',
+                                               fast_start=True)
+
+            if (not self.is_generator_on(gen)
+                and not self.was_generator_on(gen)
+                and self._data['elements']['generator'][gen][
+                    'min_up_time'] <= 1)
+            )
+
+    def is_generator_on(self, gen: str) -> bool:
+        if gen not in self._data['elements']['generator']:
+            raise ValueError(
+                "Generator `{}` is not part of this model!".format(gen))
+
+        gen_dict = self._data['elements']['generator'][gen]
+        if 'fixed_commitment' in self._data['elements']['generator'][gen]:
+            return gen_dict['fixed_commitment']['values'][0] > 0
+        elif 'commitment' in gen_dict:
+            return gen_dict['commitment']['values'][0] > 0
+
+        else:
+            raise ModelError("Cannot find commitment status "
+                             "for generator `{}`!".format(gen))
+
+    def was_generator_on(self, gen: str) -> bool:
+        return self._data['elements']['generator'][gen]['initial_status'] > 0
+
+    @property
+    def available_renewables(self):
+        return sum(self.get_max_power_output(gen)
+                   for gen, _ in self.elements(element_type='generator',
+                                               generator_type='renewable'))
+
+    @property
+    def on_offs(self):
+        return sum(self.is_generator_on(gen) != self.was_generator_on(gen)
+                   for gen, _ in self.elements(element_type='generator',
+                                               generator_type='thermal'))
+
+    @property
+    def on_off_ramps(self):
+        return sum(gen_data['pg']['values'][0]
+                   for gen, gen_data in self.elements(element_type='generator',
+                                                      generator_type='thermal')
+                   if self.is_generator_on(gen) != self.was_generator_on(gen))
+
+    @property
+    def nominal_ramps(self):
+        return sum(abs(gen_data['pg']['values'][0]
+                       - gen_data['initial_p_output'])
+                   for gen, gen_data in self.elements(element_type='generator',
+                                                      generator_type='thermal')
+                   if self.is_generator_on(gen) == self.was_generator_on(gen))
+
+    @property
+    def price(self):
+        if self.total_demand > 0:
+            return ((self.total_costs / self.total_demand)
+                    * (60 / self.duration_minutes))
+
+        else:
+            return 0.
+
+    @property
+    def quickstart_generators(self):
+        return [gen for gen, _ in self.elements(element_type='generator',
+                                                fast_start=True)]
+
+    @property
+    def quickstart_capable(self):
+        quick_starts = set(self.quickstart_generators)
+
+        return {gen: gen in quick_starts
+                for gen, _ in self.elements(element_type='generator',
+                                            generator_type='thermal')}
+
+    @property
+    def fuels(self):
+        return {gen: gen_data['fuel']
+                for gen, gen_data in self._data['elements']['generator'].items()}
+
+    @property
+    def thermal_states(self):
+        return {gen: self.is_generator_on(gen)
+                for gen, _ in self.elements(element_type='generator',
+                                            generator_type='thermal')}
+
+    def get_generator_cost(self, gen):
+        gen_data = self._data['elements']['generator'][gen]
+
+        return (gen_data['commitment_cost']['values'][0]
+                + gen_data['production_cost']['values'][0])
+
+    @property
+    def generator_costs(self):
+        return {gen: self.get_generator_cost(gen)
+                for gen, _ in self.elements(element_type='generator',
+                                            generator_type='thermal')}
+
+    @property
+    def curtailment(self):
+        return {gen: (self.get_max_power_output(gen)
+                      - gen_data['pg']['values'][0])
+                for gen, gen_data in self.elements(element_type='generator',
+                                                   generator_type='renewable')}
+
+    @property
+    def flows(self):
+        return {line: line_data['pf']['values'][0]
+                for line, line_data in self.elements('branch')}
+
+    @property
+    def bus_demands(self):
+        return {bus: bus_data['p_load']['values'][0]
+                for bus, bus_data in self._data['elements']['load'].items()}
+
+    @property
+    def bus_mismatches(self):
+        return {bus: bus_data['p_balance_violation']['values'][0]
+                for bus, bus_data in self._data['elements']['bus'].items()}
+
+    @property
+    def bus_LMPs(self):
+        return {bus: bus_data['lmp']['values'][0]
+                for bus, bus_data in self._data['elements']['bus'].items()}
+
+    @property
+    def storage_inputs(self):
+        return {
+            store: store_data['p_charge']['values'][0]
+            for store, store_data in self._data['elements']['storage'].items()
+            }
+
+    @property
+    def storage_outputs(self):
+        return {
+            store: store_data['p_discharge']['values'][0]
+            for store, store_data in self._data['elements']['storage'].items()
+            }
+
+    @property
+    def storage_states(self):
+        return {
+            store: store_data['state_of_charge']['values'][0]
+            for store, store_data in self._data['elements']['storage'].items()
+            }
+
+    @property
+    def storage_types(self):
+        for store, store_data in self._data['elements']['storage'].items():
+            pass
+
+        return
 
     def to_egret(self) -> EgretModel:
         return EgretModel(deepcopy(self._data))
